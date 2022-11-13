6ba8e12a78b0a78505e14faaab70a3083b8a5f86
Nermin <sehicnermin97@gmail.com>
2022-11-13 21:24:10 +1100

    Change theme colour for safari

--- components/layout/DocumentHead.tsx
+++ components/layout/DocumentHead.tsx
@@ -27,9 +27,9 @@ const DocumentHead: React.FC<Props> = ({ title, description }) => {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#191919" />
+      <meta name="msapplication-TileColor" content="#191919" />
+      <meta name="theme-color" content="#191919" />
       <title>{title}</title>
       <meta name="description" content={description} />
     </Head>
