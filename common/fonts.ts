b7e62750d35c1f64a7b3e608f1b5297a9201127e
Nermin <sehicnermin97@gmail.com>
2022-11-14 22:17:24 +1100

    Refactoring

--- common/fonts.ts
+++ common/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Space_Grotesk } from '@next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] })
+export const inter = Inter({ subsets: ['latin'] });
 export const spaceGrotesk = Space_Grotesk({ subsets: ['latin']});
