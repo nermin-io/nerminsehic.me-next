06f3a9cd2936941299f3a32fe08b1e41a63866ea
Nermin <sehicnermin97@gmail.com>
2023-03-10 20:13:37 +1100

    Reformatting

--- stitches.config.ts
+++ stitches.config.ts
@@ -1,10 +1,10 @@
 import { createStitches } from '@stitches/react';
 
 export const { styled, css, getCssText, globalCss } = createStitches({
-    media: {
-        large: '(max-width: 1195px)',
-        medium: '(max-width: 800px)',
-        small: '(max-width: 550px)',
-        extrasmall: '(max-width: 390px)'
-    }
+  media: {
+    large: '(max-width: 1195px)',
+    medium: '(max-width: 800px)',
+    small: '(max-width: 550px)',
+    extrasmall: '(max-width: 390px)',
+  },
 });
