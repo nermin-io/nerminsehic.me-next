a6512038b5be7659f45fa6e6daca07d7ef7eebdf
Nermin <sehicnermin97@gmail.com>
2023-04-09 13:13:14 +1000

    implement 'coming soon' project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { styled } from '../stitches.config';
 import Box from './Box';
 import Button from './Button';
 import type { Content } from '@prismicio/client';
@@ -10,6 +9,7 @@ import ProjectCardImage from './ProjectCardImage';
 import CardContent from './CardContent';
 import DateTag from './DateTag';
 import Card from './Card';
+import { isFilled } from '@prismicio/helpers';
 
 interface Props {
   project: Content.ProjectDocument;
@@ -20,7 +20,9 @@ const ProjectCard: React.FC<Props> = ({ project }) => {
     <Card>
       <ProjectCardImage image={project.data.image} />
       <DateTag>
-        {dayjs(project.data.published_at).format('DD MMM YYYY')}
+        {project.data.is_released
+          ? dayjs(project.data.published_at).format('DD MMM YYYY')
+          : 'Coming Soon'}
       </DateTag>
       <CardContent>
         <Box css={{ padding: '10px 0px' }}>
@@ -31,9 +33,19 @@ const ProjectCard: React.FC<Props> = ({ project }) => {
         </Box>
       </CardContent>
       <CardContent css={{ paddingBottom: 5 }}>
-        <Link href={`${project.url}`}>
-          <Button>Read Case Study</Button>
-        </Link>
+        {isFilled.contentRelationship(project.data.site) && (
+          <Link
+            href={
+              project.data.is_released
+                ? `${project.url}`
+                : `${project.data.site.url}`
+            }
+          >
+            <Button>
+              {project.data.is_released ? 'Read Case Study' : 'Sneak Peak'}
+            </Button>
+          </Link>
+        )}
       </CardContent>
     </Card>
   );
